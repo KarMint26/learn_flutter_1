399cded1f2c19ec38c5708c82963ae41cda685b3
karel <kareltrisnanto26@gmail.com>
2024-11-07 10:54:20 +0700

    update screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,20 @@
-import 'package:first_project/crud_endpoint.dart';
-import 'package:first_project/dropdown_s.dart';
-import 'package:first_project/my_bottom_nav_bar.dart';
-import 'package:first_project/alert_dialog.dart';
-import 'package:first_project/mydrawer_app.dart';
-import 'package:first_project/nav_example.dart';
-import 'package:first_project/tabbar.dart';
+import 'package:first_project/ui/register_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CrudEndpoint());
+  runApp(const RootScreen());
 }
 
-// class MyApp extends StatefulWidget {
-//   const MyApp({super.key});
+class RootScreen extends StatelessWidget {
+  const RootScreen({super.key});
 
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   int nilai = 0;
-
-//   void increment() {
-//     setState((){
-//       nilai++;
-//     });
-//   }
-
-//   void decrement() {
-//     setState((){
-//       nilai--;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text(
-//             "Counter",
-//             style: TextStyle(
-//               color: Colors.white
-//             ),
-//           ),
-//           backgroundColor: Colors.blueAccent,
-//         ),
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               "$nilai", 
-//               style: const TextStyle(fontSize: 40)
-//             ,),
-//             const SizedBox(height: 24),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//               ElevatedButton(
-//                 onPressed: () => {
-//                   setState(() {
-//                     decrement();
-//                   }),
-//                 }, 
-//                 child: const Icon(Icons.remove) 
-//               ),
-//               ElevatedButton(
-//                 onPressed: () => {
-//                   setState(() {
-//                     increment();
-//                   }),
-//                 }, 
-//                 child: const Icon(Icons.add) 
-//               )
-//             ],)
-//           ],
-//         )
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/register',
+      routes: {
+        '/register': (context) => const RegisterScreen()
+      },
+    );
+  }
+}
